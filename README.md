e478d95392c325acd6c1c58996a5c7bf3e86441d
Nicolas Neudeck <nicolas.neudeck@tum.de>
2020-04-05 18:07:46 +0200

    new user and authenctication functionality

--- README.md
+++ README.md
@@ -1,29 +1,56 @@
 # Project Storage and Retrieve Service
 
+## Authentication
+
+A Authentication Token needs to be produced first
+
+### Adding a new user
+`POST /users`
+An loged in Admin user can create a new users
+```json
+{
+    "user_name": "name",
+    "password": "password"
+}
+```
+### Seeing all users
+Also only an Admin can see users and manipulate them
+`GET /users`
+
+### Seeing one user
+Admin only
+`GET /users/<string:public_id>`
+
+### Login
+`GET /login`
+with Basic Auth username and password you can login and will be presented with a token
+Pass that token as the header:
+`-H "x-access-token:<your-token>"`
+
 ## Usage
 
 All responses will have the form
 
-'''json
+```json
 {
     "data": "Mixed type holding the content of the response",
     "message": "Description of what happened"
 }
-'''
+```
 
-Subsequent response definitions will only detail the expected value of the 'data field'
+Subsequent response definitions will only detail the expected value of the `data field`
 
 ### List all projects
 
 **Definition**
 
-'GET /projects'
+`GET /projects`
 
 **Response**
 
-- '200 OK' on success
+- `200 OK` on success
 
-'''json
+```json
 [
     {
         "identifier": "todo-app",
@@ -42,29 +69,29 @@ Subsequent response definitions will only detail the expected value of the 'data
         "finished": true
     }
 ]
-'''
+```
 
 ### Adding new Project
 
 **Definition**
 
-'POST /projects'
+`POST /projects`
 
 **Arguments**
 
-- '"identifier":string' a globally unique identifier for this project
-- '"name":string' a friendly name for this project
-- '"githup":string' web address of the github repo
-- '"resources":array' what resources were used
-- '"finished":boolean' is the project is already finished
+- `"identifier":string` a globally unique identifier for this project
+- `"name":string` a friendly name for this project
+- `"githup":string` web address of the github repo
+- `"resources":array` what resources were used
+- `"finished":boolean` is the project is already finished
 
 If a project with the given identifier already exists, the existing project will be overwritten.
 
 **Response**
 
-- '201 Created' on success
+- `201 Created` on success
 
-'''json
+```json
 {
     "identifier": "todo-app",
     "name": "Todo App",
@@ -72,18 +99,18 @@ If a project with the given identifier already exists, the existing project will
     "resources" : [],
     "finished": false
 }
-'''
+```
 
 ## Lookup project details
 
-'GET /projects/<identifier>'
+`GET /projects/<identifier>`
 
 **Response**
 
-- '404 Not Found' if the project does not exist
-- '200 OK' on success
+- `404 Not Found` if the project does not exist
+- `200 OK` on success
 
-'''json
+```json
 {
     "identifier": "todo-app",
     "name": "Todo App",
@@ -91,20 +118,20 @@ If a project with the given identifier already exists, the existing project will
     "resources" : [],
     "finished": false
 }
-'''
+```
 
 ## Delete a project
 
 **Definition**
 
-'DELETE /projects/<identifier>'
+`DELETE /projects/<identifier>`
 
 **Response**
 
-- '404 Not Found' if the project does not exist
-- '204 No Content' on success
+- `404 Not Found` if the project does not exist
+- `204 No Content` on success
 
 
 ## Exmaple
 
-'curl -H "Content-Type: application/json" -X POST -d '{"identifier": "todo-app","name": "Todo App","githup": "switch","resources" : [{"abc":"def", "def":"huid"}],"finished": false}' http://localhost:5000/projects'
+`curl -H "Content-Type: application/json" -X POST -d '{"identifier": "todo-app","name": "Todo App","githup": "switch","resources" : [{"abc":"def", "def":"huid"}],"finished": false}' http://localhost:5000/projects`
--- api.py
+++ api.py
@@ -1,10 +1,15 @@
 import markdown
 import os
 import shelve
+import uuid
+import jwt
+import datetime
 
 # Import the framework
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, request, make_response
 from flask_restful import Resource, Api, reqparse
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -12,17 +17,55 @@ app = Flask(__name__)
 # Create the API
 api = Api(app)
 
+# Authentication
+
+app.config['SECRET_KEY'] = 'thisissecret'
+
 def get_db():
-    db = getattr(g, '_database', None)
+    db = getattr(g, '_project_database', None)
     if db is None:
         db = g._database = shelve.open("projects.db")
     return db
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    db = getattr(g, '_database', None)
+    db = getattr(g, '_project_database', None)
     if db is not None:
         db.close()
+
+def get_user_db():
+    db = getattr(g, '_user_database', None)
+    if db is None:
+        db = g._database = shelve.open("users.db")
+    return db
+
+@app.teardown_appcontext
+def teardown_user_db(exception):
+    db = getattr(g, '_user_database', None)
+    if db is not None:
+        db.close()
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return {'message': 'Token is missing!'}, 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            shelf = get_user_db()
+            current_user = shelf[data['public_id']]
+            print(current_user['user_name'])
+        except:
+            return {'message': 'Token is invalid!'}, 401
+
+        return f(current_user, *args, **kwargs)
+    return decorated
 
 @app.route("/")
 def home():
@@ -45,20 +88,120 @@ def index():
         # Convert to HTML
         return markdown.markdown(content)
 
+class UserList(Resource):
+    @token_required
+    def get(current_user, self):
+        if not current_user['admin']:
+            return {'message': 'no admin', 'data': {}}, 401
+        shelf = get_user_db()
+        keys = list(shelf.keys())
+
+        users = []
+
+        for key in keys:
+            users.append(shelf[key])
+
+        return {'message': 'Success', 'data': users}, 200
+    @token_required
+    def post(current_user, self):
+        if not current_user['admin']:
+            return {'message': 'no admin', 'data': {}}, 401
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_name', required=True)
+        parser.add_argument('password', required=True)
+
+        # Parse the arguments into an object
+        args = parser.parse_args()
+
+        hashed_password = generate_password_hash(args['password'], method='sha256')
+        args['password'] = hashed_password
+        args['public_id'] = str(uuid.uuid4())
+        args['admin'] = False
+        shelf = get_user_db()
+        shelf[args['public_id']] = args
+
+        return {'message': 'User created', 'data': args}, 201
+
+class User(Resource):
+    @token_required
+    def get(current_user, self, public_id):
+        if not current_user['admin']:
+            return {'message': 'no admin', 'data': {}}, 401
+        shelf = get_user_db()
+
+        # If the key does not exist in the data store, return a 404 error.
+        if not (public_id in shelf):
+            return {'message': 'User not found', 'data': {}}, 404
+
+        return {'message': 'User found', 'data': shelf[public_id]}, 200
+    @token_required
+    def put(current_user, self, public_id):
+        if not current_user['admin']:
+            return {'message': 'no admin', 'data': {}}, 401
+        shelf = get_user_db()
+
+        # If the key does not exist in the data store, return a 404 error.
+        if not (public_id in shelf):
+            return {'message': 'Project not found', 'data': {}}, 404
+
+        user = shelf[public_id]
+        user['admin'] = True
+        del shelf[public_id]
+        shelf[public_id] = user
+        return {'message': 'User has been promoted', 'data': {}}, 200
+    @token_required
+    def delete(current_user, self, public_id):
+        if not current_user['admin']:
+            return {'message': 'no admin', 'data': {}}, 401
+        shelf = get_user_db()
+
+        # If the key does not exist in the data store, return a 404 error.
+        if not (public_id in shelf):
+            return {'message': 'Project not found', 'data': {}}, 404
+
+        del shelf[public_id]
+        return {'message': 'User has been deleted', 'data': {}}, 204
+
+class Login(Resource):
+    def get(self):
+        auth = request.authorization
+
+        if not auth or not auth.username or not auth.password:
+            return make_response('Could not verify', 401, {'WWW-Authenticate':'Basic Rrealm="Login required!"'})
+
+        shelf = get_user_db()
+        keys = list(shelf.keys())
+
+        user = None
+        for key in keys:
+            if shelf[key]['user_name'] == auth.username:
+                user = shelf[key]
+
+        if user is None:
+            return make_response('Could not verify', 401, {'WWW-Authenticate':'Basic Rrealm="Login required!"'})
+
+        if check_password_hash(user.password, auth.password):
+            token = jwt.encode({'public_id': user['public_id'], 'exp' : datetime.datetime.utcnow() + datetime.timedelta(hours=3)}, app.config['SECRET_KEY'])
+            return {'message': 'Access granted', 'data': token.decode('UTF-8')}, 200
+
+        return make_response('Could not verify', 401, {'WWW-Authenticate':'Basic Rrealm="Login required!"'})
 
 class ProjectList(Resource):
-    def get(self):
+    @token_required
+    def get(current_user, self):
         shelf = get_db()
         keys = list(shelf.keys())
 
         projects = []
 
         for key in keys:
-            projects.append(shelf[key])
+            if shelf[key]['public_id'] == current_user['public_id']:
+                projects.append(shelf[key])
 
         return {'message': 'Success', 'data': projects}, 200
 
-    def post(self):
+    @token_required
+    def post(current_user, self):
         parser = reqparse.RequestParser()
 
         parser.add_argument('identifier', required=True)
@@ -70,13 +213,16 @@ class ProjectList(Resource):
         # Parse the arguments into an object
         args = parser.parse_args()
 
+        args['public_id'] = current_user['public_id']
+
         shelf = get_db()
         shelf[args['identifier']] = args
 
         return {'message': 'Project added', 'data': args}, 201
 
 class Project(Resource):
-    def get(self, identifier):
+    @token_required
+    def get(current_user, self, identifier):
         shelf = get_db()
 
         # If the key does not exist in the data store, return a 404 error.
@@ -85,18 +231,41 @@ class Project(Resource):
 
         return {'message': 'Project found', 'data': shelf[identifier]}, 200
 
-    def delete(self, identifier):
+    @token_required
+    def put(current_user, self, identifier):
         shelf = get_db()
 
         # If the key does not exist in the data store, return a 404 error.
         if not (identifier in shelf):
             return {'message': 'Project not found', 'data': {}}, 404
 
+        project = shelf[identifier]
+        if project['public_id'] != current_user['public_id']:
+            return {'message': 'This is not your project', 'data': {}}, 401
+        project['finished'] = True
+        del shelf[identifier]
+        shelf[identifier] = project
+        return {'message': 'Project is finished', 'data': {}}, 200
+
+    @token_required
+    def delete(current_user, self, identifier):
+        shelf = get_db()
+
+        # If the key does not exist in the data store, return a 404 error.
+        if not (identifier in shelf):
+            return {'message': 'Project not found', 'data': {}}, 404
+
+        project = shelf[identifier]
+        if project['public_id'] != current_user['public_id']:
+            return {'message': 'This is not your project', 'data': {}}, 401
         del shelf[identifier]
         return '', 204
 
 api.add_resource(ProjectList, '/projects')
 api.add_resource(Project, '/projects/<string:identifier>')
+api.add_resource(UserList, '/users')
+api.add_resource(User, '/users/<string:public_id>')
+api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
     app.run(debug=True)
